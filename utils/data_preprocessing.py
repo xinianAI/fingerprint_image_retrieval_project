022d622767a6f7a6e79d555c5390b76b1a746200
gaoxiang <948367165@qq.com>
2022-04-14 17:12:41 +0800

    Upload the data_processing model

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -3,15 +3,13 @@ import string
 import os
 import datetime
 
-path1 = r'../data_sets/raw_data/TZ_同指.txt'
-path2 = r'../data_sets/raw_data/TZ_同指200_乱序后_Data.txt'
-path3 = r'../data_sets/raw_data/TZ_异指.txt'
-# myfile1 = open(path1)
-# myfile2 = open(path2)
-# myfile3 = open(path3)
-len1 = 1000
-len2 = 400
-len3 = 20000
+# path1 = r'../data_sets/raw_data/TZ_同指.txt'
+# path2 = r'../data_sets/raw_data/TZ_同指200_乱序后_Data.txt'
+# path3 = r'../data_sets/raw_data/TZ_异指.txt'
+
+# len1 = 1000
+# len2 = 400
+# len3 = 20000
 
 
 class Data(object):
@@ -22,15 +20,14 @@ class Data(object):
             self.points = points
 
 
-# 输入图像文件，输出为numpy数组
+# 输入图像文件，输出为数据类Data数组
 def getData(path, len):
-    t_s = datetime.datetime.now()
+    # t_s = datetime.datetime.now()
     myfile = open(path)
-    # lines = len(myfile.readlines())
-    data_N = [Data() for i in range(1, len)]
+    data_N = [Data() for i in range(1, len+1)]
+    i = 0
     while 1:
         lines = myfile.readlines(len)
-        i = 0
         if not lines:
             break
         for line in lines:
@@ -42,17 +39,19 @@ def getData(path, len):
                 data_N[i].id = row[0]
             data_N[i].point_number = row[1]
             num = int(data_N[i].point_number)
-            data_N[i].points = [[int(row[i]), int(row[i+1]), int(row[i+2])] for i in range(2, num*3 + 2, 3)]
+            data_N[i].points = [[int(row[j]), int(row[j+1]), int(row[j+2])] for j in range(2, num*3 + 2, 3)]
             # print(data_N[i].id)
             # print(data_N[i].point_number)
             # print(data_N[i].points)
             i += 1
-    t_e = datetime.datetime.now()
-    print(t_e - t_s)
+    # t_e = datetime.datetime.now()
+    # print(t_e - t_s)
+
     myfile.close()
+    return data_N
 
 
-if __name__ == '__main__':
-    getData(path1, len1)
-    getData(path2, len2)
-    getData(path3, len3)
+# if __name__ == '__main__':
+#     tz_data_sets = getData(path1, len1)
+#     tz_shuffule_data_sets = getData(path2, len2)
+#     yz_data_sets = getData(path3, len3)
---
+++ utils/feature_extraction.py
@@ -0,0 +1,16 @@
+from data_preprocessing import getData
+
+# 路径
+path1 = r'../data_sets/raw_data/TZ_同指.txt'
+path2 = r'../data_sets/raw_data/TZ_同指200_乱序后_Data.txt'
+path3 = r'../data_sets/raw_data/TZ_异指.txt'
+
+# 数据集长度
+len1 = 1000
+len2 = 400
+len3 = 20000
+
+# 数据集
+tz_data_sets = getData(path1, len1)
+# tz_shuffule_data_sets = getData(path2, len2)
+# yz_data_sets = getData(path3, len3)
